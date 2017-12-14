b0202aac4263310f54c42482bd3475cab2e8cdfc
YmraNaissur <daily.spb@yandex.ru>
2017-12-14 21:56:15 +0300

    Performed validation in Controller class according to section 15.

--- SpringMVCMaster/WebContent/WEB-INF/view/section15/customer-form.jsp
+++ SpringMVCMaster/WebContent/WEB-INF/view/section15/customer-form.jsp
@@ -12,7 +12,7 @@
 		<form:form action="processForm" modelAttribute="customer">
 			First name: <form:input path="firstName" />
 			<br/><br/>
-			Last name: <form:input path="lastName" />
+			Last name (*): <form:input path="lastName" />
 			<form:errors path="lastName" cssClass="error" />
 			<br/><br/>
 			<input type="submit" value="Submit" />
--- SpringMVCMaster/src/com/naissur/section15/CustomerController.java
+++ SpringMVCMaster/src/com/naissur/section15/CustomerController.java
@@ -1,7 +1,11 @@
 package com.naissur.section15;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,7 +14,15 @@ public class CustomerController {
 	
 	@RequestMapping("/showForm")
 	public String showForm(Model model) {
-		model.addAttribute(new Customer());
+		model.addAttribute("customer", new Customer());
 		return "section15/customer-form";
+	}
+	
+	@RequestMapping("/processForm")
+	public String processForm(@Valid
+							  @ModelAttribute("customer") Customer customer,
+							  BindingResult result) {
+		
+		return result.hasErrors() ? "section15/customer-form" : "section15/customer-confirmation";
 	}
 }
